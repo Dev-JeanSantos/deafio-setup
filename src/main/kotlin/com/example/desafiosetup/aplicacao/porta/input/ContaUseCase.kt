1bf48d45d518325589d2010ff6afcc900d076428
freonzx <freonzx@gmail.com>
2022-11-07 02:06:09 -0300

    feat: commit transaction to dynamo

--- src/main/kotlin/com/example/desafiosetup/aplicacao/porta/input/ContaUseCase.kt
+++ src/main/kotlin/com/example/desafiosetup/aplicacao/porta/input/ContaUseCase.kt
@@ -1,8 +1,11 @@
 package com.example.desafiosetup.aplicacao.porta.input
 
+import com.example.desafiosetup.adapter.output.dynamodb.entidade.CorrentistaModel
 import com.example.desafiosetup.adapter.web.v1.request.TransferenciaRequest
 import com.example.desafiosetup.adapter.web.v1.response.CorrentistaResponse
+import java.math.BigDecimal
 
 interface ContaUseCase {
-    fun transferir(transferenciaRequest:TransferenciaRequest):CorrentistaResponse
+    fun transferir(transferenciaRequest:TransferenciaRequest): CorrentistaResponse
+    fun processar(valor: BigDecimal, debito: CorrentistaModel, credito: CorrentistaModel): CorrentistaResponse
 }
--- src/main/kotlin/com/example/desafiosetup/aplicacao/servico/ContaService.kt
+++ src/main/kotlin/com/example/desafiosetup/aplicacao/servico/ContaService.kt
@@ -1,12 +1,12 @@
 package com.example.desafiosetup.aplicacao.servico
 
+import com.example.desafiosetup.adapter.output.dynamodb.entidade.CorrentistaModel
 import com.example.desafiosetup.adapter.web.v1.request.TransferenciaRequest
 import com.example.desafiosetup.adapter.web.v1.response.CorrentistaResponse
-import com.example.desafiosetup.aplicacao.dominio.modelo.Conta
 import com.example.desafiosetup.aplicacao.dominio.modelo.Erro
+import com.example.desafiosetup.aplicacao.dominio.modelo.toModel
 import com.example.desafiosetup.aplicacao.porta.input.ContaUseCase
 import com.example.desafiosetup.aplicacao.porta.output.CorrentistaRepositorioPorta
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.util.*
@@ -14,34 +14,34 @@ import java.util.*
 @Service
 class ContaService(
     private val correntistaRepositorioPorta: CorrentistaRepositorioPorta,
-    @Autowired
-    private val transferenciaService: TransferenciaService
 ): ContaUseCase {
     override fun transferir(transferenciaRequest: TransferenciaRequest): CorrentistaResponse {
         val contaDebito = correntistaRepositorioPorta.buscarCorrentistaPorNumeroConta(transferenciaRequest.contaDebito)
+
         if (contaDebito.conta.saldo >= transferenciaRequest.valor){
             val contaCredito = correntistaRepositorioPorta.buscarCorrentistaPorNumeroConta(transferenciaRequest.contaCredito)
-            val debitoSucesso = processar(transferenciaRequest.valor, contaDebito.toConta(),contaCredito.toConta())
-            return contaCredito.toResponse()
+            val debitoSucesso = processar(transferenciaRequest.valor, contaDebito, contaCredito)
+
+            return debitoSucesso
         }
+
         println("não entrou!")
         return  contaDebito.toResponse()
     }
 
+    override fun processar(valor: BigDecimal, debito: CorrentistaModel, credito: CorrentistaModel): CorrentistaResponse {
+        val debitoConta = debito.toConta()
+        val creditoConta = credito.toConta()
+
+        // Utiliza os metodos de creditar e debitar da classe criada
+        debitoConta.debitar(valor)
+        creditoConta.creditar(valor)
+
+        // Cria copias dos registros existentes no banco e sobreescreve os saldos
+        val debitoNovoSaldo = debito.copy(conta = debitoConta.toModel())
+        val creditoNovoSaldo = credito.copy(conta = creditoConta.toModel())
+
+        return correntistaRepositorioPorta.transferirValoresEntreContas(debitoNovoSaldo, creditoNovoSaldo).toResponse()
+    }
 }
 
-fun processar(valor: BigDecimal?, debito: Conta?, credito: Conta?){
-
-    if (Objects.isNull(valor)){
-        Erro.obrigatorio("Valor da Transferência ")
-    }
-    if (Objects.isNull(debito)){
-        Erro.obrigatorio("Conta Débito ")
-    }
-    if (Objects.isNull(credito)){
-        Erro.obrigatorio("Conta Crédito ")
-    }
-
-    debito?.debitar(valor)
-    credito?.creditar(valor)
-}
